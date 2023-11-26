09db719a4f5dacc1fc233fdea26f0b0576b70aa4
camdzic <camdzic.ofisl@proton.me>
2023-11-26 14:22:50 +0100

    fix heartbeat

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,74 @@
 import WebSocket from "ws";
 const sleep = (ms: number) =>
-  new Promise<void>((resolve) => setTimeout(resolve, ms));
+	new Promise<void>((resolve) => setTimeout(resolve, ms));
 
 type EventType = "READY" | "MESSAGE_CREATE";
 
 class Base {
-  private token: string;
-  private ws: WebSocket | null;
+	private token: string;
+	private ws: WebSocket | null;
 
-  constructor(token: string) {
-    this.token = token;
-    this.ws = null;
+	constructor(token: string) {
+		this.token = token;
+		this.ws = null;
 
-    this.connect();
-  }
+		this.connect();
+	}
 
-  private connect() {
-    this.ws = new WebSocket("wss://gateway.discord.gg/?v=10&encording=json");
+	private connect() {
+		this.ws = new WebSocket("wss://gateway.discord.gg/?v=10&encording=json");
 
-    this.ws.on("open", () => {
-      console.log("WebSocket opened");
-    });
-    this.ws.on("close", (code: number) => {
-      console.log("WebSocket closed with code: " + code);
-      setTimeout(() => this.connect(), 5000);
-    });
-    this.ws.on("message", (event: string) => this.message(event));
-  }
+		this.ws.on("open", () => {
+			console.log("WebSocket opened");
+		});
+		this.ws.on("close", (code: number) => {
+			console.log("WebSocket closed with code: " + code);
+			setTimeout(() => this.connect(), 5000);
+		});
+		this.ws.on("message", (event: string) => this.message(event));
+	}
 
-  private message(event: string) {
-    const data = JSON.parse(event);
+	private message(event: string) {
+		const data = JSON.parse(event);
 
-    if (data.d?.heartbeat_interval) {
-      const interval = data.d.heartbeat_interval;
-      this.heartbeat(interval);
+		if (data.d?.heartbeat_interval) {
+			const interval = data.d.heartbeat_interval;
+			this.heartbeat(interval);
 
-      this.ws?.send(
-        JSON.stringify({
-          op: 2,
-          d: {
-            token: this.token,
-            properties: {
-              os: "",
-              browser: "",
-            },
-            compress: false,
-          },
-        }),
-      );
-    }
+			this.ws?.send(
+				JSON.stringify({
+					op: 2,
+					d: {
+						token: this.token,
+						properties: {
+							os: "",
+							browser: "",
+						},
+						compress: false,
+					},
+				})
+			);
+		}
 
-    switch (data.t as EventType) {
-      case "READY":
-        console.log("Hi");
-        break;
-    }
-  }
+		switch (data.t as EventType) {
+			case "READY":
+				console.log("Hi");
+				break;
+		}
+	}
 
-  private async heartbeat(interval: number) {
-    while (true) {
-      this.ws?.send(
-        JSON.stringify({
-          op: 1,
-          d: null,
-        }),
-      );
+	private async heartbeat(interval: number) {
+		while (this.ws && this.ws.readyState === WebSocket.OPEN) {
+			this.ws?.send(
+				JSON.stringify({
+					op: 1,
+					d: null,
+				})
+			);
 
-      await sleep(interval);
-    }
-  }
+			await sleep(interval);
+		}
+	}
 }
 
 new Base("TOKEN");
